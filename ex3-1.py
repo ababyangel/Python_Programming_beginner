a6daa27f929ee2ab80fe78dc2d31c3b4a593f2fc
ababyangel <ababyangel123@hotmail.com>
2025-05-30 23:28:40 +0800

    Create ex3-1.py

---
+++ ex3-1.py
@@ -0,0 +1,19 @@
+name = "嚴鵬飛"
+age = 12
+print(name, age)
+
+x, y, z = 0, 0, 7
+print(x, y, z)
+
+a = b = c = 6
+print(a, b, c)
+
+x = 10      # Eric
+y = 3.14    # is
+z = "Hello" # SB
+print(type(x), type(y), type(z))
+
+x = 10
+print(type(x))  # int
+x = "Python"
+print(type(x))  # str
